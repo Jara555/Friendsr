0f4cc11b2d72af51cc90f510a5d977a15aa77575
Jara555 <jaralinders@gmail.com>
2018-04-25 13:07:49 +0200

    Version 1 working app

--- app/src/main/java/com/jara/friendsr/MainActivity.java
+++ app/src/main/java/com/jara/friendsr/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.GridView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,86 +21,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        // initialize views
-//        EditText editText = (EditText) findViewById(R.id.editText);
-//
-//        loadFromSharedPrefs();
+        // get grid view
+        GridView gridView = (GridView) findViewById(R.id.gridView);
 
+        // initialize list with friends
         createFriends();
 
+        // initialize and set adapter to grid view
         FriendsAdapter adapter = new FriendsAdapter(this, R.layout.grid_item, friends);
-
-        GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setAdapter(adapter);
 
+        // create grid click listener item
+        GridItemClickListener gridClick = new GridItemClickListener();
+        gridView.setOnItemClickListener(gridClick);
+
+    }
+
+    /* Directs to the the profile activity of clicked item */
+    private class GridItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            // get friend of clicked position
+            Friend clickedFriend = (Friend) parent.getItemAtPosition(position);
+            System.out.println(clickedFriend.getName());
+
+            // create intent and pass clicked friend through
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            intent.putExtra("clicked_friend", clickedFriend);
+            startActivity(intent);
+        }
     }
 
     public void createFriends() {
 
         int id_ross = getResources().getIdentifier("ross", "drawable", "com.jara.friendsr");
-        Friend Ross = new Friend("Ross Geller", "I love dinosaurs", id_ross);
+        Friend Ross = new Friend("Ross", "I love dinosaurs", id_ross);
         friends.add(Ross);
 
         int id_joey = getResources().getIdentifier("joey", "drawable", "com.jara.friendsr");
-        Friend Joey = new Friend("Joey Tribbiani", "I love pizza", id_joey);
+        Friend Joey = new Friend("Joey", "I love pizza", id_joey);
         friends.add(Joey);
 
         int id_rachel = getResources().getIdentifier("rachel", "drawable", "com.jara.friendsr");
-        Friend Rachel = new Friend("Rachel Green", "I love fashion", id_rachel);
+        Friend Rachel = new Friend("Rachel", "I love fashion", id_rachel);
         friends.add(Rachel);
 
         int id_monica = getResources().getIdentifier("monica", "drawable", "com.jara.friendsr");
-        Friend Monica = new Friend("Monica Geller", "I love cleaning", id_monica);
+        Friend Monica = new Friend("Monica", "I love cleaning", id_monica);
         friends.add(Monica);
 
         int id_chandler= getResources().getIdentifier("chandler", "drawable", "com.jara.friendsr");
-        Friend Chandler = new Friend("Chandler Bing", "I love jokes", id_chandler);
+        Friend Chandler = new Friend("Chandler", "I love jokes", id_chandler);
         friends.add(Chandler);
 
         int id_phoebe = getResources().getIdentifier("phoebe", "drawable", "com.jara.friendsr");
-        Friend Phoebe = new Friend("Phoebe Buffay", "I love massages", id_phoebe);
+        Friend Phoebe = new Friend("Phoebe", "I love massages", id_phoebe);
         friends.add(Phoebe);
     }
-
-//    /* This method is connected to a button in the main activity and will
-//        direct to the the profile activity when clicked */
-//    public void goToProfile(View view) {
-//
-//        String text = editText.getText().toString();
-//
-//        // intent: huidige + navigeer naar
-//        Intent intent = new Intent(this, ProfileActivity.class);
-//
-//        intent.putExtra("Text", text);
-//
-//        startActivity(intent);
-//
-//        // close first activity (no copy will remain in this way)
-//        finish();
-//    }
-//
-//    /* Adds things to shared prefs */
-//    public void saveToSharedPrefs(View view) {
-//
-//        String text = editText.getText().toString();
-//
-//        SharedPreferences prefs = this.getSharedPreferences("settings", this.MODE_PRIVATE);
-//        SharedPreferences.Editor editor = prefs.edit();
-//
-//        editor.putString("text", text);
-//        editor.commit();
-//
-//    }
-//
-//    public void loadFromSharedPrefs() {
-//
-//        SharedPreferences prefs = this.getSharedPreferences("settings", this.MODE_PRIVATE);
-//
-//        String textRestored = prefs.getString("text", null);
-//        if (textRestored != null) {
-//            editText.setText(textRestored);
-//        }
-//
-//    }
-
 }
